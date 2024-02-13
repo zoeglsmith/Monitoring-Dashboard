02e8ac5a72d1f217ecb7d92f7d0d178cc6433a39
smizo424 <smizo424@student.otago.ac.nz>
2024-02-14 00:43:50 +1300

    connected to server

--- script.js
+++ script.js
@@ -1,54 +1,41 @@
+// Client-side JavaScript code
 document
   .getElementById("websiteForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
     var websiteUrl = document.getElementById("websiteUrl").value;
-    fetchMetrics(websiteUrl);
+    fetchMetrics(
+      "http://localhost:3000/fetch?url=" + encodeURIComponent(websiteUrl)
+    );
   });
 
-function fetchMetrics(websiteUrl) {
-  var startTime = new Date().getTime(); // Get the current time before making the request
+function fetchMetrics(proxyUrl) {
+  var startTime = new Date().getTime();
 
-  // Fetch the webpage content using fetch API
-  fetch(websiteUrl)
+  fetch(proxyUrl)
     .then((response) => {
-      // Check if response is successful
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
-
-      // Calculate the response time
-      var endTime = new Date().getTime(); // Get the current time after receiving the response
+      var endTime = new Date().getTime();
       var responseTime = endTime - startTime;
-
-      // Return the response and response time
       return {
         response: response,
         responseTime: responseTime,
+        websiteUrl: decodeURIComponent(proxyUrl.split("=")[1]), // Extract websiteUrl from proxyUrl
       };
     })
-    .then(({ response, responseTime }) => {
-      // Get the status code of the response
+    .then(({ response, responseTime, websiteUrl }) => {
       var statusCode = response.status;
-
-      // Parse the HTML content using DOMParser
       return response.text().then((html) => {
         var parser = new DOMParser();
         var doc = parser.parseFromString(html, "text/html");
 
-        // Get the title of the webpage
         var title = doc.querySelector("title").textContent;
-
-        // Count the number of paragraphs
         var paragraphs = doc.querySelectorAll("p").length;
-
-        // Count the number of images
         var images = doc.querySelectorAll("img").length;
-
-        // Count the number of links
         var links = doc.querySelectorAll("a").length;
 
-        // Display the metrics
         var monitoringResults = document.getElementById("monitoringResults");
         monitoringResults.innerHTML =
           "<p>Monitoring website: <strong>" + websiteUrl + "</strong></p>";
@@ -58,8 +45,6 @@ function fetchMetrics(websiteUrl) {
           "<p>Status Code: <strong>" + statusCode + "</strong></p>";
         monitoringResults.innerHTML +=
           "<p>Response Time: <strong>" + responseTime + " ms</strong></p>";
-
-        // Content metrics
         monitoringResults.innerHTML +=
           "<p>Number of Paragraphs: <strong>" + paragraphs + "</strong></p>";
         monitoringResults.innerHTML +=
@@ -69,7 +54,6 @@ function fetchMetrics(websiteUrl) {
       });
     })
     .catch((error) => {
-      // Handle errors
       console.error("There was a problem with the fetch operation:", error);
       var monitoringResults = document.getElementById("monitoringResults");
       monitoringResults.innerHTML =
